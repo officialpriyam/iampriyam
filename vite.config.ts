8efc8bfa8569a30fbe7257c7b0ad4bb81a58c0be
priam mishra <officialpriyammishra2006@gmail.com>
2026-06-10 18:13:12 +0530

    add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -10,6 +11,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro({ preset: "vercel" }),
     react(),
     tailwindcss(),
   ],
